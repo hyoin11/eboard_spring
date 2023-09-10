c5af2d17eb922f789160c7b56045343ec6c3cf44
hyoin <kslove1111@gmail.com>
2023-09-10 17:54:38 +0900

    init

---
+++ src/main/java/com/hyo11/eboard/EboardApplication.java
@@ -0,0 +1,13 @@
+package com.hyo11.eboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EboardApplication.class, args);
+	}
+
+}
